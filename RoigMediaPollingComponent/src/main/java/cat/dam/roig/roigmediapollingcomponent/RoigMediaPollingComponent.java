9fc4cda84e79845bbe4620201b6e466b1c337a32
xesgan <h4ku.era@gmail.com>
2025-12-02 18:55:52 +0100

    Initial Maven project structure for MediaPolling component

---
+++ RoigMediaPollingComponent/src/main/java/cat/dam/roig/roigmediapollingcomponent/RoigMediaPollingComponent.java
@@ -0,0 +1,13 @@
+
+package cat.dam.roig.roigmediapollingcomponent;
+
+/**
+ *
+ * @author metku
+ */
+public class RoigMediaPollingComponent {
+
+    public static void main(String[] args) {
+        
+    }
+}
